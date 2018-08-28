2df412c196aa8da653e99ba101e11a2d7b4cbb0e
Mohsen <mabbasidesign@yahoo.com>
2018-08-28 14:10:23 -0600

    adding route parameter

--- src/App.js
+++ src/App.js
@@ -14,11 +14,12 @@ class App extends Component {
     return (
       <div className="content">
         <Switch>
+          <Route path="/products/:id" component={ProductDetails} />
           <Route
               path="/products"
               render={props => <Products sortBy="newest" {...props} />}
             />
-          <Route path="/post" component={Posts} />
+          <Route path="/post/:year/:month" component={Posts} />
           <Route path="/admin" component={Dashboard} />
           <Route path="/" component={Home} />
         </Switch>
